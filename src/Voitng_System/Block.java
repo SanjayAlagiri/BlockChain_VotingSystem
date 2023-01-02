443cec5dc37c347e9f82ac15d52feaf74f1e6e16
SanjayAlagiri <sanjayalagiri6@gmail.com>
2023-01-02 21:33:51 +0530

    first commit

---
+++ src/Voitng_System/Block.java
@@ -0,0 +1,5 @@
+package Voitng_System;
+
+public class Block {
+
+}
---
+++ src/Voitng_System/BlockChain.java
@@ -0,0 +1,9 @@
+package Voitng_System;
+
+public class BlockChain {
+    public static void main(String[] args) {
+        String Name = "Rohit";
+        int hash1 = Name.hashCode();
+        System.out.println(hash1);
+    }
+}
